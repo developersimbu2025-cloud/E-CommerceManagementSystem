d0002bbd346505535625c6f8c8043e9966ddccf3
Silambarsan <developersimbu2025@gmail.com>
2025-09-19 16:03:54 +0530

    adminadd

--- Admin/src/pages/Products.tsx
+++ Admin/src/pages/Products.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import SearchInput from "../component/ui/search";
 import ProductTable from "./ProductTable";
 
 const Products: React.FC = () => {
-   const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="ant-pro-table space-y-5 ">
-      <div className="ant-pro-card-search p-5 bg-white rounded">
-        <SearchInput value={searchTerm} placeholder="Enter Product Name" />
-      </div>
+      <div className="ant-pro-card-search p-5 bg-white rounded">searchTerm</div>
       <div className="ant-pro-card-table p-5 bg-white rounded space-y-5">
         <ProductTable />
       </div>
